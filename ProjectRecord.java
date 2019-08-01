df0222dd398bf5a2be341a1e90930e5f2939baf3
Pankajan Satkunam <pankajan05@gmail.com>
2019-08-01 15:43:50 +0530

    Add files via upload
    This file is the projectstore have the details of the project store

---
+++ ProjectRecord.java
@@ -0,0 +1,13 @@
+package  com.company.track;
+
+public class ProjectRecord {
+
+    public int numberOfAllocatedResources;
+    String id;
+    String title;
+    String clientId;
+    int duration;
+    int rate;
+    int numberOfAllocatedResorces;
+    String type; //fixed/variable
+}
